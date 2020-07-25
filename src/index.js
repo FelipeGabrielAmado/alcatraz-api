4015fac77ec5ce94abcce47027fb90458281887f
Felipe Amado <felipe@dblinks.com.br>
2020-07-25 11:24:39 -0300

    Update CORS policy again

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 
 // Routes
 app.use(require('./routes/index'));
-res.setHeader('Access-Control-Allow-Origin');
+res.setHeader('Access-Control-Allow-Origin', 'https://alcatraz-api.herokuapp.com');
 res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 res.setHeader('Access-Control-Allow-Credentials', true);
