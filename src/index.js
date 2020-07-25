cd2deabfab53c6d1783a165f2fe45b9ebb99a0e1
Felipe Amado <felipe@dblinks.com.br>
2020-07-25 11:21:40 -0300

    Update CORS policy

--- src/index.js
+++ src/index.js
@@ -5,11 +5,15 @@ const app = express();
 
 // middlewares
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
 // Routes
 app.use(require('./routes/index'));
+res.setHeader('Access-Control-Allow-Origin');
+res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+res.setHeader('Access-Control-Allow-Credentials', true);
 
 app.listen(process.env.PORT || 3000);
 console.log('Server on port', 3000);
