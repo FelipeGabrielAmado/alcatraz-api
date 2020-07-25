c50fd43eaaf43f9877f9a3bd6d8c2fc63ab0d721
Felipe Amado <felipe@dblinks.com.br>
2020-07-25 11:34:25 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,6 +8,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Header',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).send({});
+    }
+    next();
+});
+
 // Routes
 app.use(require('./routes/index'));
 
