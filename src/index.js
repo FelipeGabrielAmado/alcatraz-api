122fdaffab6377f1307d9f5f0f1b86dc05d32824
Felipe Amado <felipe@dblinks.com.br>
2020-07-25 11:46:30 -0300

    Grant access to Front page

--- src/index.js
+++ src/index.js
@@ -6,21 +6,10 @@ const app = express();
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-        'Access-Control-Allow-Header',
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    );
 
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-        return res.status(200).send({});
-    }
-    next();
-});
+
+app.use(cors('https://alcatraz-front.herokuapp.com'));
 
 // Routes
 app.use(require('./routes/index'));
